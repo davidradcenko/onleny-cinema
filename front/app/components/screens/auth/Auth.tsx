f20f2c30290b34a643309e8a0df2d7307de2b165
David Radchenko <davidradchenko2@gmail.com>
2023-10-12 02:18:28 -0700

    lesn 12 min 10.22

---
+++ front/app/components/screens/auth/Auth.tsx
@@ -0,0 +1,15 @@
+import React, { FC, useState } from 'react'
+import { useAuthRedirect } from './useAuthRedirect'
+import { useAuth } from '@/hooks/useAuth'
+
+const Auth: FC = () => {
+    useAuthRedirect()
+
+const {isLoading} = useAuth()
+const [type,setType] = useState<'login' | 'register'>('login')
+  return (
+    <div>Auth</div>
+  )
+}
+
+export default Auth

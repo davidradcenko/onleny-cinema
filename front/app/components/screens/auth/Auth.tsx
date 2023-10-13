fbd75af6346ef979977a4bb30723df068a0fd040
David Radchenko <davidradchenko2@gmail.com>
2023-10-13 03:23:14 -0700

    16:30

--- front/app/components/screens/auth/Auth.tsx
+++ front/app/components/screens/auth/Auth.tsx
@@ -1,6 +1,10 @@
 import React, { FC, useState } from "react";
 import { useAuthRedirect } from "./useAuthRedirect";
 import { useAuth } from "@/hooks/useAuth";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { IAuthInput } from "./auth.interface";
+import styles from "./Auth.module.scss";
+import Meta from "@/utils/meta/Meta";
 
 const Auth: FC = () => {
   useAuthRedirect();
@@ -9,7 +13,30 @@ const Auth: FC = () => {
 
   const [type, setType] = useState<"login" | "register">("login");
 
-  return <div>Auth</div>;
+  const {
+    register: registerInput,
+    handleSubmit,
+    formState,
+    reset,
+  } = useForm<IAuthInput>({
+    mode: "onChange",
+  });
+
+  const login = (data: any) => {};
+  const register = (data: any) => {};
+
+  const onSubmit: SubmitHandler<IAuthInput> = (data) => {
+    if (type === "login") login(data);
+    else if (type === "register") register(data);
+    reset();
+  };
+  return (
+    <Meta description="Auth" title="Auth">
+      <section className={styles.wrapper}>
+        <form onSubmit={handleSubmit(onSubmit)}></form>
+      </section>
+    </Meta>
+  );
 };
 
 export default Auth;
